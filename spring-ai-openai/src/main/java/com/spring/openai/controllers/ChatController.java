e0ecec6abc7bcc24c4b2faf03a05aac3f63a75c0
kumarmanish562 <qmanishkumar562@gmail.com>
2026-09-20 16:26:46 +0530

    feat(ai): integrate and configure OpenAI and Ollama

---
+++ spring-ai-openai/src/main/java/com/spring/openai/controllers/ChatController.java
@@ -0,0 +1,29 @@
+package com.spring.openai.controllers;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ChatController {
+
+    @Autowired
+    private ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient=builder.build();
+    }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(
+            @RequestParam(value = "q", required = true) String q
+    ) {
+        var resultResponse = chatClient.prompt(q).call().content();
+        return ResponseEntity.ok(resultResponse);
+    }
+}
